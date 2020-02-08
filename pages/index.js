80fad68b63af2b71ace084134f3d6b38dea0fb8f
eduardo rivas <r.eduardo92@gmail.com>
2020-02-08 12:14:08 -0600

    second init

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
 import Layout from '../components/layout';
 import VideoHero from '../components/ui/VideoHero';
-// import Carousell from '../components/ui/carousel/carousel';
 import Contact from '../components/contact';
 
 const IndexPage = () => (
   <Layout>
     <VideoHero />
     <Contact />
-    {/* <Carousell /> */}
   </Layout>
 );
 
